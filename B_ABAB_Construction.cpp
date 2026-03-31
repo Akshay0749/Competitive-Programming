196e2216581e2406fa1896b3c4079d9b87a2aaf0
Akshay0749 <ghuleakshay2405@gmail.com>
2026-03-31 18:07:27 +0530

    add solution: B - ABAB Construction

---
+++ B_ABAB_Construction.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ent endl
+typedef long long ll;
+typedef long double ld;
+
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+
+#define FOR(i, a, b) for (int i=a; i<(b); i++)
+#define F0R(i, a) for (int i=0; i<(a); i++)
+#define FORd(i,a,b) for (int i = (b)-1; i >= a; i--)
+#define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
+#define trav(a,x) for (auto& a : x)
+#define uid(a, b) uniform_int_distribution<int>(a, b)(rng)
+
+// Problem Statement
+/*
+    
+*/
+
+// Small Observatins
+/*
+ 
+*/
+
+/*
+ 
+*/
+
+// Claims on algo 
+/*  
+    
+ 
+*/
+
+void solve() {
+    ll n; cin >> n;
+    string s; cin >> s;
+    ll l = 0, r = n - 1;
+    F0R(i, n) {
+        char lChar = (l % 2 == 0 ? 'a' : 'b');
+        char rChar = (r % 2 == 0 ? 'a' : 'b');
+
+        if(s[i] == lChar) {
+            l++;
+        }
+        else if(s[i] == rChar) {
+            r--;
+        } 
+        else if(s[i] == '?') {
+            if(lChar == rChar) {
+                l++;
+            } else {
+                if(i+1 < n && s[i+1] == lChar) {
+                    r--;
+                } else {
+                    l++;
+                }
+            }
+        }
+        else {
+            cout << "NO" << ent;
+            return;
+        }
+    }
+    cout << "YES" << ent;
+}
+
+// Golden Rules
+/*
+    Solutions are simple.
+
+    Proofs are simple.
+
+    Implementations are simple.
+*/
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+
+    int T = 1;
+    cin >> T;
+    while(T--) {
+        solve();
+    }
+
+    return 0;
+}
