03d99f181493aa9dcdf03dbabd4154593be4103d
Akshay0749 <ghuleakshay2405@gmail.com>
2026-05-28 02:05:07 +0530

    new que

---
+++ Comfortable_Seating.cpp
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ent endl
+typedef long long ll;
+typedef long double ld;
+
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+const int MOD = 1e9 + 7;
+
+#define FOR(i, a, b) for (int i=a; i<(b); i++)
+#define F0R(i, a) for (int i=0; i<(a); i++)
+#define FORd(i,a,b) for (int i = (b)-1; i >= a; i--)
+#define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
+#define trav(a,x) for (auto& a : x)
+#define uid(a, b) uniform_int_distribution<int>(a, b)(rng)
+
+ll nCr(long long n, long long r) {
+    if(r > n) return 0;
+    if(r > n - r) r = n - r;
+    long long res = 1;
+    for(ll i = 0; i < r; i++) {
+       res = res * (n - i);
+       res = res / (i + 1);
+    }
+    return res;
+}
+
+// Problem Statement
+/*
+    
+*/
+
+// Small Observatins
+/*
+ 
+ 
+
+ 
+ 
+*/
+
+// Claims on algo 
+/*  
+    
+ 
+*/
+
+void solve() {
+    bool ok = true;
+    ll n; cin >> n;
+    vl a(n);
+    set<ll> s;
+    ll cnt = 0;
+    F0R(i, n) {
+        cin >> a[i];
+        s.insert(a[i]);
+    }
+    ll mn = *min_element(a.begin(), a.end());
+    F0R(i, n) {
+        if(a[i] == mn) cnt++;
+    }
+    if(s.size() == n) ok = false;
+    if(cnt < 2) ok = false;
+
+    if(ok) cout << "YES" << ent;
+    else cout << "NO" << ent;
+   
+}
+
+// Golden Rules
+/*
+    Solutions are simple.
+
+    Proofs are simple.
+
+    Implementations are simple.
+*/
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+
+    int T = 1;
+    cin >> T;
+    while(T--) {
+        solve();
+    }
+
+    return 0;
+}
