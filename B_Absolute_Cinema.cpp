ffb736d34fadd062c7db85f25535c9710dd89e77
Akshay0749 <ghuleakshay2405@gmail.com>
2026-06-05 18:36:25 +0530

    new que

---
+++ B_Absolute_Cinema.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ent endl
+typedef long long ll;
+typedef long double ld;
+
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+const int MOD = 1e9 + 7;
+
+#define FOR(i, a, b) for (int i=a; i<(b); i++)
+#define F0R(i, a) for (int i=0; i<(a); i++)
+#define FORd(i,a,b) for (int i = (b)-1; i >= a; i--)
+#define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
+#define trav(a,x) for (auto& a : x)
+#define uid(a, b) uniform_int_distribution<int>(a, b)(rng)
+
+ll nCr(long long n, long long r) {
+    if(r > n) return 0;
+    if(r > n - r) r = n - r;
+    long long res = 1;
+    for(ll i = 0; i < r; i++) {
+       res = res * (n - i);
+       res = res / (i + 1);
+    }
+    return res;
+}
+
+// Problem Statement
+/*
+    
+*/
+
+// Small Observatins
+/*
+ 
+ 
+
+ 
+ 
+*/
+
+// Claims on algo 
+/*  
+    
+ 
+*/
+
+void solve() {
+    ll n; cin >> n; 
+    vl a(n), b(n);
+    F0R(i, n) cin >> a[i];
+    F0R(i, n) cin >> b[i];
+    ll ans = 0;
+    F0R(i, n) {
+        ll mx = max(a[i], b[i]);
+        ll mn = min(a[i], b[i]);
+        ans += mx;
+        a[i] = mn;
+    }
+    ans += *max_element(a.begin(), a.end());
+
+    cout << ans << ent;
+   
+}
+
+// Golden Rules
+/*
+    Solutions are simple.
+
+    Proofs are simple.
+
+    Implementations are simple.
+*/
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+
+    int T = 1;
+    cin >> T;
+    while(T--) {
+        solve();
+    }
+
+    return 0;
+}
