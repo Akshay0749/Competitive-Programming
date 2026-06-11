f1943974471bd829eb893e1c1bbd5f25e3ec4262
Akshay0749 <ghuleakshay2405@gmail.com>
2026-06-11 19:34:45 +0530

    new que

---
+++ C_First_or_Second.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ent endl
+typedef long long ll;
+typedef long double ld;
+
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+const int MOD = 1e9 + 7;
+
+#define FOR(i, a, b) for (int i=a; i<(b); i++)
+#define F0R(i, a) for (int i=0; i<(a); i++)
+#define FORd(i,a,b) for (int i = (b)-1; i >= a; i--)
+#define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
+#define trav(a,x) for (auto& a : x)
+#define uid(a, b) uniform_int_distribution<int>(a, b)(rng)
+
+ll nCr(long long n, long long r) {
+    if(r > n) return 0;
+    if(r > n - r) r = n - r;
+    long long res = 1;
+    for(ll i = 0; i < r; i++) {
+       res = res * (n - i);
+       res = res / (i + 1);
+    }
+    return res;
+}
+
+// Problem Statement
+/*
+    
+*/
+
+// Small Observatins
+/*
+ 
+ 
+*/
+
+// Claims on algo 
+/*  
+    
+ 
+*/
+
+void solve() {
+    ll n; cin >> n;
+    vl a(n);
+    F0R(i, n) cin >> a[i];
+
+    vl suff(n + 1, 0);
+    FORd(i, 0, n) {
+        suff[i] = suff[i + 1] + a[i];
+    }
+
+    vl absPref(n + 1, 0);
+    FOR(i, 1, n) absPref[i+1] = absPref[i] + abs(a[i]);
+
+    ll ans = LLONG_MIN;
+
+    // 0th index el is left
+    ans = max(ans, -suff[1]);
+
+    // except 0, 1 to n - 1 can be left
+    FOR(i, 1, n) {
+        ans = max(ans, a[0] + absPref[i] - suff[i + 1]);
+    }
+
+    cout << ans << ent;
+   
+}
+
+// Golden Rules
+/*
+    Solutions are simple.
+
+    Proofs are simple.
+
+    Implementations are simple.
+*/
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+
+    int T = 1;
+    cin >> T;
+    while(T--) {
+        solve();
+    }
+
+    return 0;
+}
