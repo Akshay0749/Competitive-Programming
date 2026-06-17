b6141556a25344ea7f2b94fe8b094f9a0464d840
Akshay0749 <ghuleakshay2405@gmail.com>
2026-06-17 19:47:45 +0530

    new que

---
+++ A_Dragons.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ent endl
+typedef long long ll;
+typedef long double ld;
+
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+const int MOD = 1e9 + 7;
+
+#define FOR(i, a, b) for (int i=a; i<(b); i++)
+#define F0R(i, a) for (int i=0; i<(a); i++)
+#define FORd(i,a,b) for (int i = (b)-1; i >= a; i--)
+#define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
+#define trav(a,x) for (auto& a : x)
+#define uid(a, b) uniform_int_distribution<int>(a, b)(rng)
+
+ll nCr(long long n, long long r) {
+    if(r > n) return 0;
+    if(r > n - r) r = n - r;
+    long long res = 1;
+    for(ll i = 0; i < r; i++) {
+       res = res * (n - i);
+       res = res / (i + 1);
+    }
+    return res;
+}
+
+// Problem Statement
+/*
+    
+*/
+
+// Small Observatins
+/*
+ 
+ 
+
+ 
+ 
+*/
+
+// Claims on algo 
+/*  
+    
+ 
+*/
+
+void solve() {
+    ll s, n; cin >> s >> n;
+    vector<pair<ll, ll>> a;
+    F0R(i, n) {
+        ll x, y; cin >> x >> y;
+        a.push_back({x, y});
+    }   
+    sort(a.begin(), a.end());
+
+    for(auto &[x, y]: a) {
+        if(s > x) {
+            s += y;
+        }
+        else {
+            cout << "NO";
+            return;
+        }
+    }
+
+    cout << "YES"; 
+   
+}
+
+// Golden Rules
+/*
+    Solutions are simple.
+
+    Proofs are simple.
+
+    Implementations are simple.
+*/
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+
+    int T = 1;
+    // cin >> T;
+    while(T--) {
+        solve();
+    }
+
+    return 0;
+}
