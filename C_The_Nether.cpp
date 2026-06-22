79c56e3051ba408296268c1d3148f5f419c4e88b
Akshay0749 <ghuleakshay2405@gmail.com>
2026-06-22 20:05:03 +0530

    add solution for "The Nether" problem

---
+++ C_The_Nether.cpp
@@ -0,0 +1,106 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ent endl
+typedef long long ll;
+typedef long double ld;
+
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+const int MOD = 1e9 + 7;
+
+#define FOR(i, a, b) for (ll i=a; i<(b); i++)
+#define F0R(i, a) for (int i=0; i<(a); i++)
+#define FORd(i,a,b) for (int i = (b)-1; i >= a; i--)
+#define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
+#define trav(a,x) for (auto& a : x)
+#define uid(a, b) uniform_int_distribution<int>(a, b)(rng)
+
+ll nCr(long long n, long long r) {
+    if(r > n) return 0;
+    if(r > n - r) r = n - r;
+    long long res = 1;
+    for(ll i = 0; i < r; i++) {
+       res = res * (n - i);
+       res = res / (i + 1);
+    }
+    return res;
+}
+
+// Problem Statement
+/*
+    
+*/
+
+// Small Observatins
+/*
+ 
+ 
+
+ 
+ 
+*/
+
+// Claims on algo 
+/*  
+    
+ 
+*/
+
+void solve() {
+    ll n; cin >> n;
+    ll ans = 0;
+    vl v[n + 1]; // 2d vector
+
+    FOR(i, 1, n + 1) {
+        cout << "? " << i << ' ' << n << " ";
+        for(ll j = 1; j <= n; j++) cout << j << ' ';
+        cout << endl;
+        ll x; cin >> x;
+        ans = max(ans, x);
+        v[x].push_back(i);
+    }
+
+    ll s = v[ans][0];
+    vl path = {s};
+
+    for(int i = ans - 1; i >= 1; i--) {
+        for(auto it: v[i]) {
+            cout << '?' << ' ' << s << ' ' << 2 << ' ' << s << ' ' << it << ent;
+            ll x; cin >> x;
+            if(x == 2) {
+                s = it;
+                path.push_back(s);
+                break;
+            }
+        }
+    }
+    
+
+    cout << '!' << ' ' << path.size() << ' ';
+    trav(it, path) cout << it << ' ';
+    cout << ent;
+
+}
+
+// Golden Rules
+/*
+    Solutions are simple.
+
+    Proofs are simple.
+
+    Implementations are simple.
+*/
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+
+    int T = 1;
+    cin >> T;
+    while(T--) {
+        solve();
+    }
+
+    return 0;
+}
